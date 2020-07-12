712ae08835c53e232b463024306d8d79b1df56a0
SAFNAS <iamsafnas@gmail.com>
2020-07-12 22:47:40 +0530

    basic app to show the workout

---
+++ src/com/safnas/springdemo/MyApp.java
@@ -0,0 +1,16 @@
+package com.safnas.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		//create the object
+		BaseballCoach baseballCoach = new BaseballCoach();
+		
+		//call the object
+		System.out.println(baseballCoach.getDailyWorkout());
+		
+		
+
+	}
+
+}

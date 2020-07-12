22e47f3832328957c70eb6ee1e3f241a81119627
SAFNAS <iamsafnas@gmail.com>
2020-07-12 22:51:57 +0530

    implemented and overridden

--- src/com/safnas/springdemo/BaseballCoach.java
+++ src/com/safnas/springdemo/BaseballCoach.java
@@ -1,7 +1,8 @@
 package com.safnas.springdemo;
 
-public class BaseballCoach {
+public class BaseballCoach implements Coach{
 	
+	@Override
 	public String getDailyWorkout(){
 		return "Spend 30 mins on batting practice";
 	}

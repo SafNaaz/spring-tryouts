d1303ae36e075c776b0a8aea92da351abfefc304
Safnas <iamsafnas@gmail.com>
2020-07-12 23:36:29 +0530

    ran first spring application

---
+++ src/com/safnas/springdemo/HelloSpringApp.java
@@ -0,0 +1,22 @@
+package com.safnas.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		//load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// retrieve spring bean from container
+		Coach theCoach = context.getBean("myCoach",Coach.class);
+		
+		//call method on bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		//close context
+		context.close();
+
+	}
+
+}
